cd8ff280e721f1f873fd4fafb068ee6c2f429e3d
Annt <annguyenthe.work@gmail.com>
2022-07-02 17:16:22 +0700

    install prettier

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+
+const route = require('./routes');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+// HTTP logger
+// app.use(morgan('combined'))
+
+// Template engine
+app.engine(
+    'hbs',
+    exphbs.engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+
+// Set path views
+app.set('views', path.join(__dirname, 'resources/views'));
+
+route(app);
+
+app.listen(port, () =>
+    console.log('Examp app listening at http://localhost:${port}'),
+);
