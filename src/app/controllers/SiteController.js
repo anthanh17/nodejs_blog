c26c9f8a48aeb11d26e1ad04111afd8b763219a5
Annt <annguyenthe.work@gmail.com>
2022-07-04 23:02:11 +0700

    CRUD Read from database

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,21 +1,20 @@
 const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 class SiteController {
     // [GET] /
-    home(req, res) {
-        //res.render('home');
-        Course.find({}, function (err, course) {
-            if (!err) {
-                res.json(course);
-            } else {
-                res.status(400).json({ error: 'Error!!' });
-            }
-        });
+    home(req, res, next) {
+        Course.find({})
+            .then((courses) => {
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses),
+                });
+            })
+            .catch(next);
     }
-
     // [GET] /search
     search(req, res) {
         res.render('search');
     }
 }
-
+// CRUD <=> Create Read Update Delete Data
 module.exports = new SiteController();
